2dae471b231703b5625027e6ad9f21e4c835680e
TasinFarhanMC <tasinfarhan1016@gmail.com>
2025-03-28 10:22:30 +0600

    Fixed start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-cd $(dirname $0)
+cd "$(dirname "$0")" || exit
 
-if [ -z $VIRTUAL_ENV ]; then
+if [ -z "$VIRTUAL_ENV" ]; then
   if [ ! -d "venv" ]; then
     echo "Creating virtual environment..."
     python3 -m venv venv
   fi
 
   echo "Checking dependencies..."
+  venv/bin/pip install -r requirements.txt
+
   echo "Running game..."
   venv/bin/python src/main.py
 else
